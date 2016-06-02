4fced99f26cd6066b7e90a4ad9566916c7f8b566
Nam <namlu1205@gmail.com>
2016-06-02 11:53:07 +1000

    Checkpoint 23 completed

--- foundation/24-interfaces/src/com/bloc/interfaces/people/Albert.java
+++ foundation/24-interfaces/src/com/bloc/interfaces/people/Albert.java
@@ -4,9 +4,22 @@ package com.bloc.interfaces.people;
  *	ASSIGNMENT:
  *	Have Albert implement the SalsaDancer interface
 /************************************************/
+import com.bloc.interfaces.people.hobbies.*;
 
-public class Albert extends Person {
+public class Albert extends Person implements SalsaDancer{
 	public Albert() {
 		super("Albert", "Cobb", "Male", 1.8d, 72d, "Green");
+	}
+	
+	public void putOnShoes(){
+		
+	}
+	
+	public void findAPartner(){
+		
+	}
+	
+	public void salsa(){
+		
 	}
 }
--- foundation/24-interfaces/src/com/bloc/interfaces/people/John.java
+++ foundation/24-interfaces/src/com/bloc/interfaces/people/John.java
@@ -4,9 +4,22 @@ package com.bloc.interfaces.people;
  *	ASSIGNMENT:
  *	Have John implement the Driver interface
 /************************************************/
+import com.bloc.interfaces.people.hobbies.*;
 
-public class John extends Person {
+public class John extends Person implements Driver{
 	public John() {
 		super("John", "Smith", "Male", 1.7d, 69d, "Brown");
+	}
+	
+	public void getInCar(){
+		
+	}
+	
+	public void startEngine(){
+		
+	}
+	
+	public void driveFast(){
+		
 	}
 }
