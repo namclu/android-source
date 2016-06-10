d4d6250314362ae49b7713a8086a62f7d931b0ca
Nam <namlu1205@gmail.com>
2016-06-10 19:14:02 +1000

    Checkpoint 27 completed

--- foundation/28-recursion/src/com/bloc/recursion/RecursionUtils.java
+++ foundation/28-recursion/src/com/bloc/recursion/RecursionUtils.java
@@ -20,6 +20,51 @@ public class RecursionUtils extends Object {
 	 	 *	Implement this method, the return value must
 	 	 *	change
 		/************************************************/
-		return 0;
+		//Base case
+		if(numbers.size() == 1){
+			return numbers.get(0);
+		}
+		while(numbers.size() > 1){
+			if(numbers.get(0) < numbers.get(numbers.size() - 1)){
+				numbers.set(0, numbers.get(numbers.size() - 1));
+			}
+			return findMaxRecursively(numbers.subList(0, (numbers.size() - 1)));
+		}
+		return numbers.get(0);
+
+		/*
+		//Base case
+		if(numbers.size() == 1){
+			return numbers.get(0);
+		} else{
+			int max1 = findMaxRecursively(numbers.subList(0, numbers.size() / 2));
+			int max2 = findMaxRecursively(numbers.subList(numbers.size() / 2, numbers.size()));
+			return max1 > max2 ? max1 : max2;
+		}
+		*/
+		
+		/* 
+		int tempMax = numbers.get(0);
+		
+		//Base case
+		if(numbers.size() == 1){
+			return numbers.get(0);
+		} else{
+			List<Integer> numbersList = new ArrayList<Integer>(numbers);
+			tempMax = (numbers.get(0) > numbers.get(numbers.size() - 1) ? numbers.get(0) : numbers.get(numbers.size() - 1));
+			return findMaxRecursively(numbers.subList(0, numbers.siz() - 1));
+			
+		} */
+		
+		/* int tempMax = numbers.get(0);
+		
+		while(numbers.size() > 1){
+			if(tempMax < numbers.get(numbers.size() - 1)){
+				tempMax = numbers.get(numbers.size() - 1);
+				System.out.println("tempMax = " + tempMax);
+			}
+			return findMaxRecursively(numbers.subList(0, (numbers.size() - 1)));
+		}
+		return tempMax; */
 	}
 }
