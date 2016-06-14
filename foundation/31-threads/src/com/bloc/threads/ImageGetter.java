667111757378563e9dd039df26327078a6de8ffd
Nam <namlu1205@gmail.com>
2016-06-14 10:13:33 +1000

    Checkpoint 30 - in progress #1

--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -1,5 +1,10 @@
 package com.bloc.threads;
 
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+
 public class ImageGetter extends Thread {
 
 	/*
@@ -12,12 +17,19 @@ public class ImageGetter extends Thread {
 	 * @param openWhenCompleted if `true`, the image will be opened
 	 *		  as soon it is downloaded, `false` otherwise
 	 */
+	 public URL mUrl;
+	 public boolean mOpenWhenCompleted = false;
+	 
 	/************************************************
  	 *	ASSIGNMENT:
  	 *	Implement this constructor for ImageGetter.
  	 *	The variables passed into it must impact the `run()` method's
  	 *	behavior as described above.
 	/************************************************/
+	public ImageGetter(URL url, boolean openWhenCompleted){
+		mUrl = url;
+		mOpenWhenCompleted = openWhenCompleted;
+	}
 
 	@Override
 	public void run() {
@@ -26,5 +38,56 @@ public class ImageGetter extends Thread {
  		 *	Perform the work found in `Main.java` in this
  		 *	Thread instead.
 		/************************************************/
+		try {
+			File existingImage = new File("google_logo.png");
+			if (existingImage.exists()) {
+				existingImage.delete();
+			}
+			//URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
+			BufferedImage bufferedImage = ImageIO.read(mUrl);
+			File outputfile = new File("google_logo.png");
+			ImageIO.write(bufferedImage, "png", outputfile);
+			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+				Runtime.getRuntime().exec("open google_logo.png");
+			} else {
+				Runtime.getRuntime().exec("google_logo.png");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		File logo = new File("google_logo.png");
+		//boolean exists = false;
+		try {
+			//exists = logo.exists();
+			mOpenWhenCompleted = logo.exists();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		//if (exists) {
+		if (mOpenWhenCompleted) {
+			System.out.println("/************************/");
+			System.out.println("/*                      */");
+			System.out.println("/*                      */");
+			System.out.println("/* Download that image  */");
+			System.out.println("/* on a separate thread */");
+			System.out.println("/*                      */");
+			System.out.println("/*                      */");
+			System.out.println("/************************/\n");
+		} else {
+			System.out.println("/************************/");
+			System.out.println("/*                      */");
+			System.out.println("/*                      */");
+			System.out.println("/* Nice work, you pass! */");
+			System.out.println("/*                      */");
+			System.out.println("/*                      */");
+			System.out.println("/************************/\n");
+		}
 	}
 }
--- foundation/31-threads/src/com/bloc/threads/Main.java
+++ foundation/31-threads/src/com/bloc/threads/Main.java
@@ -9,33 +9,15 @@ public class Main extends Object {
 
 	public static void main(String [] args) {
 
+		new ImageGetter(new URL("https://www.google.com/images/srpr/logo11w.png"), true).start();
+		new ImageGetter(new URL("https://www.google.com/images/srpr/logo11w.png"), false).start();
 		/************************************************
 		 *	ASSIGNMENT:
 		 *	Perform the following block of code on a separate
 		 *	Thread, use the ImageGetter class.
 		/************************************************/
 
-		try {
-			File existingImage = new File("google_logo.png");
-			if (existingImage.exists()) {
-				existingImage.delete();
-			}
-			URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
-			BufferedImage bufferedImage = ImageIO.read(url);
-			File outputfile = new File("google_logo.png");
-			ImageIO.write(bufferedImage, "png", outputfile);
-			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
-				Runtime.getRuntime().exec("open google_logo.png");
-			} else {
-				Runtime.getRuntime().exec("google_logo.png");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+		
 
 		/************************************************
 		 *	ASSIGNMENT:
@@ -43,32 +25,6 @@ public class Main extends Object {
 		 *	Thread, use the ImageGetter class.
 		/************************************************/
 
-		File logo = new File("google_logo.png");
-		boolean exists = false;
-		try {
-			exists = logo.exists();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-
-		if (exists) {
-			System.out.println("/************************/");
-			System.out.println("/*                      */");
-			System.out.println("/*                      */");
-			System.out.println("/* Download that image  */");
-			System.out.println("/* on a separate thread */");
-			System.out.println("/*                      */");
-			System.out.println("/*                      */");
-			System.out.println("/************************/\n");
-		} else {
-			System.out.println("/************************/");
-			System.out.println("/*                      */");
-			System.out.println("/*                      */");
-			System.out.println("/* Nice work, you pass! */");
-			System.out.println("/*                      */");
-			System.out.println("/*                      */");
-			System.out.println("/************************/\n");
-		}
+		
 	}
 }
